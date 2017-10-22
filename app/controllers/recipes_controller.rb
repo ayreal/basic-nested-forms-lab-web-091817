453112dbfebb0572db1d32d7f9deb691a27455a0
Ariel Herman <aeh.herman@gmail.com>
2017-10-22 18:23:20 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,28 @@ class RecipesController < ApplicationController
   end
 
   def new
+    # creating 2 recipe ingredient instances here makes the form create two different ingredient objects associated with this recipe
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    #byebug
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save!
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes:[:name, :quantity])
+  end
+
 end
